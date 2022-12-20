0c645c4a7f5ff01571e3667a068392a7d41498ac
Timothy Moore <mtimothy984@gmail.com>
2022-12-20 05:26:53 -0800

    Fix not bumping earliest key for dau

--- runners/stats/daily_active_users.py
+++ runners/stats/daily_active_users.py
@@ -60,5 +60,13 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
                 )
             )
 
-        logging.debug(f"redis delete {key=}")
-        await redis.delete(key)
+        async with redis.pipeline() as pipe:
+            pipe.multi()
+
+            logging.debug(f"redis delete {key=}")
+            await pipe.delete(key)
+
+            logging.debug(f"redis set {earliest_key=} {unix_date + 1}")
+            await pipe.set(earliest_key, bytes(str(unix_date + 1), "ascii"))
+
+            await pipe.execute()
