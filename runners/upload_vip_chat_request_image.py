830b74aab01cb0595eeeadf3b20d92982143c19a
Timothy Moore <mtimothy984@gmail.com>
2023-04-19 15:18:13 -0700

    Use s3 to place vip chat request image

--- runners/upload_vip_chat_request_image.py
+++ runners/upload_vip_chat_request_image.py
@@ -5,8 +5,8 @@ from graceful_death import GracefulDeath
 from .process_journey_background_image import get_jpg_settings, get_webp_settings
 from images import process_image, ImageTarget, ProcessImageAbortedException
 import logging
-import os
 from jobs import JobCategory
+from temp_files import temp_file
 
 
 category = JobCategory.HIGH_RESOURCE_COST
@@ -39,35 +39,43 @@ TARGETS = [
 
 async def execute(itgs: Itgs, gd: GracefulDeath):
     """Updates the vip_chat_request_image_uid in redis to match the file located
-    at tmp/vip_chat_request_image.jpg
+    in s3 under man/vip_chat_request_image.jpg
 
     Args:
         itgs (Itgs): the integration to use; provided automatically
         gd (GracefulDeath): the signal tracker; provided automatically
     """
-    path = os.path.join("tmp", "vip_chat_request_image.jpg")
-    try:
-        image = await process_image(
-            path,
-            TARGETS,
-            itgs=itgs,
-            gd=gd,
-            max_width=16384,
-            max_height=16384,
-            max_area=8192 * 8192,
-            max_file_size=1024 * 1024 * 512,
-            name_hint="journey_background_image",
+    files = await itgs.files()
+    with temp_file() as path:
+        with open(path, "wb") as f:
+            await files.download(
+                f,
+                bucket=files.default_bucket,
+                key="man/vip_chat_request_image.jpg",
+                sync=True,
+            )
+        try:
+            image = await process_image(
+                path,
+                TARGETS,
+                itgs=itgs,
+                gd=gd,
+                max_width=16384,
+                max_height=16384,
+                max_area=8192 * 8192,
+                max_file_size=1024 * 1024 * 512,
+                name_hint="journey_background_image",
+            )
+        except ProcessImageAbortedException:
+            logging.warning("vip_chat_request_image.png processing was aborted; retry")
+            return
+
+        redis = await itgs.redis()
+        await redis.set("vip_chat_request_image_uid", image.uid)
+
+        logging.info(
+            f"vip_chat_request_image.png processing complete; assigned uid {image.uid}"
         )
-    except ProcessImageAbortedException:
-        logging.warning("vip_chat_request_image.png processing was aborted; retry")
-        return
-
-    redis = await itgs.redis()
-    await redis.set("vip_chat_request_image_uid", image.uid)
-
-    logging.info(
-        f"vip_chat_request_image.png processing complete; assigned uid {image.uid}"
-    )
 
 
 if __name__ == "__main__":
@@ -76,10 +84,6 @@ if __name__ == "__main__":
     import logging.config
 
     async def main():
-        if not os.path.exists(os.path.join("tmp", "vip_chat_request_image.jpg")):
-            print("vip_chat_request_image.jpg not found; aborting")
-            return
-
         gd = GracefulDeath()
         with open("logging.yaml") as f:
             logging_config = yaml.safe_load(f)
