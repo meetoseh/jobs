e0ab2eac18a1205795b487ad4e9696690133b31c
Timothy Moore <mtimothy984@gmail.com>
2023-05-03 11:12:57 -0700

    Reset emotion votes daily, positive emotions

--- runners/repopulate_emotions.py
+++ runners/repopulate_emotions.py
@@ -6,11 +6,12 @@ space to fit the prompt and response.
 This requires a transcript for each journey that is tagged; journeys without
 transcripts are skipped.
 """
+import datetime
 import os
 import re
 import secrets
 import string
-from typing import AsyncIterator, FrozenSet, List, Literal, Optional, Tuple
+from typing import AsyncIterator, FrozenSet, List, Literal, Optional, Set, Tuple
 from itgs import Itgs
 from graceful_death import GracefulDeath
 import logging
@@ -26,26 +27,40 @@ import openai
 import json
 import time
 
+from temp_files import temp_file
+
 category = JobCategory.HIGH_RESOURCE_COST
 
-
-# Unhappy emotions or feelings that you might come to Oseh to resolve
-EMOTIONS: List[Tuple[str, str]] = [
-    ("angry", "calm down"),
-    ("annoyed", "be patient"),
-    ("anxious", "be calm"),
-    ("awake", "go to sleep"),
-    ("tense", "relax"),
-    ("noisy", "simmer down"),
-    ("sad", "cheer up"),
-    ("closed", "open up"),
-    ("lost", "find yourself"),
-    ("disconnected", "connect"),
-    ("fear", "be brave"),
-    ("shame", "be proud"),
-    ("guilt", "be proud"),
+EMOTIONS: List[Tuple[str, str, Set[str]]] = [
+    ("calm", "calm down", "calm"),
+    ("compassionate", "be compassionate", {"compassion", "compassionate"}),
+    ("chill", "chill out", {"chill", "chilled"}),
+    ("satisfied", "be satisfied", {"satisfied", "satisfaction"}),
+    (
+        "relaxed",
+        "relax",
+        {"relaxed", "relaxation", "relax", "peaceful", "peace", "at peace"},
+    ),
+    ("hopeful", "be hopeful", {"hope", "hopeful", "hopefulness"}),
+    ("positive", "be positive", {"positive", "positivity"}),
+    ("focused", "be focused", {"focus", "focused"}),
+    ("energized", "be energized", {"energized", "energize", "energetic"}),
+    ("patient", "be patient", {"patient", "patience"}),
+    ("inspired", "be inspired", {"inspired", "inspiration"}),
+    ("creative", "be creative", {"creative", "creativity"}),
+    ("purpose", "find purpose", {"purpose", "purposeful", "purposefulness"}),
+    ("connected", "be connected", {"connected", "connection", "connect"}),
+    ("open", "be open", {"open", "openness"}),
+    ("free", "be free", {"free", "freedom"}),
+    ("grounded", "be grounded", {"grounded", "grounding"}),
+    ("loved", "feel loved", {"loved", "love"}),
+    ("balanced", "be balanced", {"balanced", "balance", "balanceful"}),
+    ("heard", "be heard", {"heard"}),
+    ("content", "be content", {"contentful", "content"}),
+    ("supported", "be supported", {"supported", "nurtured", "nurture", "support"}),
+    ("valued", "feel valued", {"value", "valued"}),
+    ("safe", "be safe", {"safe", "secure", "secured", "security"}),
 ]
-EMOTIONS_SET = frozenset(emotion for (emotion, _) in EMOTIONS)
 PROMPT_VERSION = "1.0.0"
 # semver for create_prompt_for_journey
 
@@ -63,7 +78,7 @@ def create_prompt_for_journey(
         transcript (Transcript): The transcript of the journey
     """
     numbered_tags_lines = []
-    for i, (emotion, _) in enumerate(EMOTIONS):
+    for i, (emotion, _, _) in enumerate(EMOTIONS):
         numbered_tags_lines.append(f"{i + 1}. {emotion}")
     numbered_tags = "\n".join(numbered_tags_lines)
 
@@ -73,7 +88,7 @@ def create_prompt_for_journey(
         {
             "role": "system",
             "content": f"""Your role in this task is to determine what feelings
-the class resolves. You must output a numbered list containing at least 2
+the class evokes. You must output a numbered list containing at least 2
 emotions. You must only choose amongst the specified emotions. The classes are
 designed such that there are always at least two matching emotions, so you must
 select at least two emotions to get full credit.
@@ -90,8 +105,8 @@ This class asks you to connect with your inner child, which is generally associa
 with feelings of being playful, happy, and care-free. Hence, the class is designed
 to resolve the following feelings:
 
-1. tense
-2. closed
+1. happy
+2. loved
 """,
         },
         {
@@ -108,12 +123,17 @@ to resolve the following feelings:
     ]
 
 
-def parse_emotions(completion: str) -> FrozenSet[str]:
+def parse_emotions(
+    completion: str, *, dropped: Optional[Set[str]] = None
+) -> FrozenSet[str]:
     """Parses the emotions from the completion provided by the
     completions api.
 
     Args:
         completion (str): the completion to parse
+        dropped (set[str], None): If specified, any dropped emotions are
+            included in this set, which can be useful for identifying emotions
+            we should have in our list but don't.
 
     Returns:
         List[str]: the selected emotion tags
@@ -154,20 +174,30 @@ def parse_emotions(completion: str) -> FrozenSet[str]:
     if not raw_emotions:
         raise ValueError(f"Invalid completion: {completion} (no emotions selected)")
 
-    valid_emotions = []
-    for emotion in raw_emotions:
-        emotion = emotion.lower().strip(string.whitespace + string.punctuation)
-        if emotion not in EMOTIONS_SET:
-            logging.debug(f"Dropping completed emotion {emotion=}: not a valid tag")
-        else:
-            valid_emotions.append(emotion)
+    valid_emotions = set()
+    for suggested_emotion in raw_emotions:
+        suggested_emotion = suggested_emotion.lower().strip(
+            string.whitespace + string.punctuation
+        )
+        found = False
+        for base, _, aliases in EMOTIONS:
+            if suggested_emotion == base or suggested_emotion in aliases:
+                valid_emotions.add(base)
+                found = True
+
+        if not found:
+            logging.debug(
+                f"Dropping completed emotion {suggested_emotion=}: not a valid tag"
+            )
+            if dropped is not None:
+                dropped.add(suggested_emotion)
 
     if len(valid_emotions) < 2:
         raise ValueError(
             f"Invalid completion: {completion} (only {len(valid_emotions)} valid emotions selected)"
         )
 
-    return valid_emotions
+    return frozenset(valid_emotions)
 
 
 def _temperature_for_attempt(attempt: int) -> float:
@@ -186,6 +216,7 @@ async def get_emotions_for_journey(
     instructor: str,
     transcript: Transcript,
     max_completion_retries: int = 5,
+    dropped: Optional[Set[str]] = None,
 ) -> FrozenSet[str]:
     """Uses chatgpt to generate the emotions for the given journey.
 
@@ -196,6 +227,12 @@ async def get_emotions_for_journey(
         description (str): The description of the journey
         instructor (str): The instructor of the journey
         transcript (Transcript): The transcript of the journey
+        max_completion_retries (int): The maximum number of times to
+            retry the completion if it fails.
+        dropped (set[str], None): If specified, any dropped emotions are
+            included in this set, which can be useful for identifying emotions
+            we should have in our list but don't.
+
 
     Returns:
         FrozenSet[str]: The emotions that were generated
@@ -221,8 +258,10 @@ async def get_emotions_for_journey(
                 api_key=openai_api_key,
             )
             logging.info(f"Got completion:\n\n{completion}")
-            emotions = parse_emotions(completion.choices[0].message.content)
-            logging.info(f"Parsed emotions: {json.dumps(emotions)}")
+            emotions = parse_emotions(
+                completion.choices[0].message.content, dropped=dropped
+            )
+            logging.info(f"Parsed emotions: {json.dumps(list(emotions))}")
             return emotions
         except Exception:
             attempt += 1
@@ -386,6 +425,7 @@ async def assign_emotions_for_journey(
     description: str,
     instructor: str,
     transcript: Optional[Transcript] = None,
+    dropped: Optional[Set[str]] = None,
 ) -> None:
     """Assigns emotions to the given journey and stores them in the database. If the
     journey already has emotions, they are deleted before the new emotions are assigned.
@@ -402,6 +442,9 @@ async def assign_emotions_for_journey(
         instructor (str): The instructor of the journey
         transcript (Transcript, None): The transcript, or None to have it fetched
             from the database
+        dropped (Set[str], None): If specified, whenever we find tags that might
+            be appropriate for content but for which there is no corresponding emotion,
+            we add them to this set
     """
     if transcript is None:
         transcript = await fetch_transcript_for_content_file(
@@ -421,8 +464,13 @@ async def assign_emotions_for_journey(
             description=description,
             instructor=instructor,
             transcript=transcript,
+            dropped=dropped,
         )
     except ValueError:
+        logging.error(
+            f"ValueError while assigning emotions for {title} by {instructor} ({journey_uid=})",
+            exc_info=True,
+        )
         slack = await itgs.slack()
         await slack.send_web_error_message(
             f"Failed to assign emotions for {title} by {instructor} (`{journey_uid}`) using transcript:\n\n```\n{transcript=}\n```",
@@ -431,7 +479,11 @@ async def assign_emotions_for_journey(
         return
 
     await store_emotions_for_journey(
-        itgs, uid=journey_uid, title=title, instructor=instructor, emotions=emotions
+        itgs,
+        uid=journey_uid,
+        title=title,
+        instructor=instructor,
+        emotions=list(emotions),
     )
 
 
@@ -461,7 +513,7 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
         values_list = ", ".join(["(?, ?)"] * len(EMOTIONS))
         response = await cursor.execute(
             f"INSERT INTO emotions (word, antonym) VALUES {values_list}",
-            [a for e in EMOTIONS for a in e],
+            [a for e in EMOTIONS for a in e[:2]],
         )
         if response.rows_affected != len(EMOTIONS):
             raise ValueError(
@@ -470,6 +522,7 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
         logging.info(f"Uploaded {len(EMOTIONS)} new emotions")
 
         logging.debug("Assigning emotions to journeys...")
+        dropped: Set[str] = set()
         async for journey_uid, content_file_uid, title, description, instructor in iter_journeys_for_emotions(
             itgs
         ):
@@ -477,6 +530,7 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
                 await _bounce()
                 return
 
+            logging.info(f"Assigning emotions for {journey_uid=}")
             await assign_emotions_for_journey(
                 itgs,
                 journey_uid=journey_uid,
@@ -484,6 +538,7 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
                 title=title,
                 description=description,
                 instructor=instructor,
+                dropped=dropped,
             )
 
         logging.info("Done repopulating emotions, fetching stats...")
@@ -501,7 +556,7 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
         )
         emotion_counts = {row[0]: row[1] for row in response.results}
 
-        for emotion, _ in EMOTIONS:
+        for emotion, _, _ in EMOTIONS:
             if emotion not in emotion_counts:
                 emotion_counts[emotion] = 0
 
@@ -513,6 +568,7 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
         }
 
         logging.info(f"Emotion counts: {json.dumps(emotion_counts, indent=2)}")
+        logging.info(f"Dropped emotions: {json.dumps(sorted(dropped), indent=2)}")
 
         if os.environ["ENVIRONMENT"] != "dev":
             slack = await itgs.slack()
@@ -520,6 +576,25 @@ async def execute(itgs: Itgs, gd: GracefulDeath):
                 f"Emotion counts:\n\n```\n{json.dumps(emotion_counts, indent=2)}\n```\n",
                 preview="Emotions repopulated successfully",
             )
+
+            if len(dropped) > 20:
+                key = f"s3_files/jobs/repopulate_emotions/dropped_{datetime.datetime.now().isoformat()}.txt"
+                with temp_file() as path:
+                    with open(path, "w") as f:
+                        for tag in sorted(dropped):
+                            print(tag, file=f)
+
+                    files = await itgs.files()
+                    with open(path, "rb") as f:
+                        await files.upload(f, bucket=files.default_bucket, key=key)
+
+                    await slack.send_ops_message(
+                        f"Tags that were dropped were stored at {key=} ({len(dropped)} emotions)"
+                    )
+            else:
+                await slack.send_ops_message(
+                    f"Dropped tags:\n\n```\n{json.dumps(sorted(dropped), indent=2)}\n```\n"
+                )
 
 
 if __name__ == "__main__":
---
+++ runners/reseed_emotion_votes.py
@@ -0,0 +1,55 @@
+from typing import List
+from itgs import Itgs
+from graceful_death import GracefulDeath
+import logging
+from jobs import JobCategory
+import random
+
+category = JobCategory.LOW_RESOURCE_COST
+
+
+async def execute(itgs: Itgs, gd: GracefulDeath):
+    """Randomly seeds some votes for emotions, so that users don't see a tiny number
+    of people selecting any given emotion. See backend/emotions/lib/emotion_users.py
+
+    Args:
+        itgs (Itgs): the integration to use; provided automatically
+        gd (GracefulDeath): the signal tracker; provided automatically
+    """
+    conn = await itgs.conn()
+    cursor = conn.cursor("none")
+
+    redis = await itgs.redis()
+    key = b"emotion_users:choices"
+
+    response = await cursor.execute("SELECT word FROM emotions")
+    emotions: List[str] = [row[0] for row in response.results or []]
+    if not emotions:
+        logging.info("No emotions, so no votes to seed")
+        await redis.delete(key)
+        return
+
+    votes = [random.randint(20, 50) for _ in emotions]
+    total_votes = sum(votes)
+
+    async with redis.pipeline(transaction=True) as pipe:
+        pipe.multi()
+        await pipe.delete(key)
+
+        for emotion, votes in zip(emotions, votes):
+            await pipe.hincrby(key, emotion.encode("utf-8"), votes)
+        await pipe.hincrby(key, b"__total", total_votes)
+        await pipe.execute()
+
+    logging.info(f"Seeded {total_votes} votes for {len(emotions)} emotions")
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def main():
+        async with Itgs() as itgs:
+            jobs = await itgs.jobs()
+            await jobs.enqueue("runners.reseed_emotion_votes")
+
+    asyncio.run(main())
