2629b9599f8cc5db447157531e19085dafcf9287
Timothy Moore <mtimothy984@gmail.com>
2023-08-31 11:04:35 -0700

    Fix redis_hgetall_ints always returning empty

--- lib/process_redis_hgetall.py
+++ lib/process_redis_hgetall.py
@@ -1,16 +1,16 @@
 from typing import Dict
 
 
-def process_redis_hgetall_ints(result: dict) -> Dict[str, int]:
+def process_redis_hgetall_ints(raw: dict) -> Dict[str, int]:
     """Parses the dict returned from redis.hgetall into a dict of str keys and int
     values. The redis library is not particularly consistent about when we get
     str or bytes, so we handle both.
     """
-    if not isinstance(result, dict):
-        raise ValueError(f"expected dict, got {type(result)}")
+    if not isinstance(raw, dict):
+        raise ValueError(f"expected dict, got {type(raw)}")
 
-    result: Dict[str, str] = dict()
-    for key, value in result.items():
+    res: Dict[str, str] = dict()
+    for key, value in raw.items():
         if not isinstance(key, (str, bytes)):
             raise ValueError(f"expected str or bytes, got {type(key)=}")
         if not isinstance(value, (str, bytes)):
@@ -21,5 +21,5 @@ def process_redis_hgetall_ints(result: dict) -> Dict[str, int]:
             int_value = int(value)
         except ValueError:
             raise ValueError(f"while processing {str_key=} and {value=}")
-        result[str_key] = int_value
-    return result
+        res[str_key] = int_value
+    return res
